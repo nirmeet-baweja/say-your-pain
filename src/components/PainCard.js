e7c53ae2c3702259dd86a4e59ec402b542257371
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-06-09 23:49:13 +0100

    PainCard component

---
+++ src/components/PainCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+
+const PainCard = ({ painItem }) => {
+  return (
+    <div className="mb-3.5 flex flex-col items-center rounded bg-white p-10">
+      <div className="lottieAnimation">
+        <Player
+          //autoplay
+          loop
+          hover
+          src={painItem.lottie}
+        >
+          <Controls
+            visible={false}
+            buttons={["play", "repeat", "frame", "debug"]}
+          />
+        </Player>
+      </div>
+      <div className="painType">{painItem.type}</div>
+    </div>
+  );
+};
+
+export default PainCard;
