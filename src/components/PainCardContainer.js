3d8f63927a1714a6e4c9b273e736f63e864e889b
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-06-10 13:35:26 +0100

    responsiveness of PainCardContainer

--- src/components/PainCardContainer.js
+++ src/components/PainCardContainer.js
@@ -6,7 +6,7 @@ const PainCardContainer = () => {
   return (
     <div className="mt-32 text-center">
       <h1 className="text-4xl">Describe your pain</h1>
-      <div className="m-auto mt-8 grid max-w-7xl grid-cols-1 gap-4 self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="m-12 grid max-w-7xl grid-cols-1 gap-4 self-center sm:grid-cols-2 xl:mx-auto lg:grid-cols-3 2xl:grid-cols-4">
         {painItems.map((painItem) => (
           <PainCard painItem={painItem} key={painItem.type} />
         ))}
