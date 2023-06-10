30d93ce9f4f1a1d57e6cdc3f4bf268101c593b53
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-06-10 22:16:27 +0100

    translation for title

--- src/components/PainCardContainer.js
+++ src/components/PainCardContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import PainCard from "./PainCard";
 import painItems from "../data/painItems.json";
+import { useTranslation } from "react-i18next";
 
 const PainCardContainer = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="mt-32 text-center">
-      <h1 className="text-4xl">Describe your pain</h1>
-      <div className="m-12 grid max-w-7xl grid-cols-1 gap-4 self-center sm:grid-cols-2 xl:mx-auto lg:grid-cols-3 2xl:grid-cols-4">
+      <h1 className="text-4xl">{t("Describe your pain")}</h1>
+      <div className="m-12 grid max-w-7xl grid-cols-1 gap-4 self-center sm:grid-cols-2 lg:grid-cols-3 xl:mx-auto 2xl:grid-cols-4">
         {painItems.map((painItem) => (
           <PainCard painItem={painItem} key={painItem.type} />
         ))}
