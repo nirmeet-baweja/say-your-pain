62cae2726c1ee29e4e6cda3c033056c700cb9ad7
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-06-10 22:24:38 +0100

    removed Suspense from App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import "./App.css";
 
-import React, { Suspense } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import PainCardContainer from "./components/PainCardContainer";
 
-// loading component for suspense fallback
-const Loader = () => (
-  <div className="App">
-    <div>loading...</div>
-  </div>
-);
-
 const App = () => {
   return (
-    <Suspense fallback={<Loader />}>
-      <div className="flex h-full w-full flex-col bg-stone-100">
-        <Navbar
-          currentLanguage="English"
-          logo={"App name"}
-          language={"language"}
-          languageCode={"languageCode"}
-        />
-        <PainCardContainer />
-      </div>
-    </Suspense>
+    <div className="flex h-full w-full flex-col bg-stone-100">
+      <Navbar
+        currentLanguage="English"
+        logo={"App name"}
+        language={"language"}
+        languageCode={"languageCode"}
+      />
+      <PainCardContainer />
+    </div>
   );
 };
 
