642fc2d28b0e3893d3a20ab245fbf06d02223806
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-06-11 20:26:18 +0100

    lazy loading PainCard

--- src/components/PainCardContainer.js
+++ src/components/PainCardContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import LazyLoad from "react-lazy-load";
 import PainCard from "./PainCard";
 import painItems from "../data/painItems.json";
 import { useTranslation } from "react-i18next";
@@ -11,7 +12,9 @@ const PainCardContainer = () => {
       <h1 className="text-4xl">{t("Describe your pain")}</h1>
       <div className="m-12 grid max-w-7xl grid-cols-1 gap-4 self-center sm:grid-cols-2 lg:grid-cols-3 xl:mx-auto 2xl:grid-cols-4">
         {painItems.map((painItem) => (
-          <PainCard painItem={painItem} key={painItem.type} />
+          <LazyLoad height={510}>
+            <PainCard painItem={painItem} key={painItem.type} />
+          </LazyLoad>
         ))}
       </div>
     </div>
